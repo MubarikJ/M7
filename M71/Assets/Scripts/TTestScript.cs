f6055dfba622ac90d1ae8405acce52b7250d3610
Mubarik Jamal <you@example.com>
2025-09-10 21:27:54 +0200

    Day 1 Part 6
    Part 1-6

---
+++ M71/Assets/Scripts/TTestScript.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class TTestScript : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
